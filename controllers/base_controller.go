e184124188d3b4e83fa2f12b0db28405f12504c2
Manas8803 <114303599+Manas8803@users.noreply.github.com>
2023-11-09 20:33:15 +0530

    Changed title in Base Route

--- controllers/base_controller.go
+++ controllers/base_controller.go
@@ -9,7 +9,7 @@ func BaseRoute(c *gin.Context) {
 		<head>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Attractive Base Route</title>
+			<title>Auth API</title>
 			<style>
 				body {
 					font-family: Arial, sans-serif;
